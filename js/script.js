fa430289eb43e3391773ee7e34318e1101bfe8cf
uday160386 <vuktales@gmail.com>
2023-10-28 23:23:03 +0800

    fixing home page issues

--- js/script.js
+++ js/script.js
@@ -26,3 +26,15 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+const ExpandingFlexCard = document.querySelectorAll('.expanding-flex-cards')
+
+
+ExpandingFlexCard.forEach(efcEl =>{
+        efcEl.querySelectorAll('.expanding-flex-cards-item:not(active)').forEach(item => {
+            item.addEventListener('click', function(e){
+                e.preventDefault()
+                efcEl.querySelector('.expanding-flex-cards-item.active').classList.remove('active')
+                this.classList.add('active')
+            })
+        })
+})
